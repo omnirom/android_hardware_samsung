71985afef3a04f22b94efb985c81dea912d67db0
Marco Magliona <marco.magliona@gmail.com>
2014-11-16 23:35:58 +0100

    hardware_samsung: fix compilation errors
    PS5: rebase after https://gerrit.omnirom.org/#/c/10449/ and https://gerrit.omnirom.org/#/c/10448/
    
    Conflicts:
    	ril/libsecril-client-sap/Android.mk
    	ril/libsecril-client/Android.mk
    	ril/xmm6260/libril/Android.mk
    	ril/xmm6262/libril/Android.mk
    
    Change-Id: I38b3aac7ef59597488bfc2a1f0227ec15cffb051

--- exynos4/hal/libcamera/SecCamera.h
+++ exynos4/hal/libcamera/SecCamera.h
@@ -35,9 +35,6 @@
 
 #include <utils/RefBase.h>
 #include <hardware/camera.h>
-#include <videodev2.h>
-#include <videodev2_exynos_camera.h>
-#include <videodev2_exynos_media.h>
 #include "sec_utils_v4l2.h"
 
 #include "SecBuffer.h"
