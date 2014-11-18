6a65e85fbaf77a0fdaa30ea40c1256a188022609
Jiangyi <sam.andrew.jiang@gmail.com>
2014-11-18 11:56:07 -0500

    hardware/samsung: Remove -lpthread from RIL makefiles
    Change-Id: Ib0893101196519790fa21ad47e0524ebec479d87

--- ril/xmm6260/libril/Android.mk
+++ ril/xmm6260/libril/Android.mk
@@ -21,7 +21,6 @@ LOCAL_CFLAGS :=
 
 LOCAL_MODULE:= libril
 
-LOCAL_LDLIBS += -lpthread
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -42,8 +41,6 @@ LOCAL_STATIC_LIBRARIES := \
 LOCAL_CFLAGS :=
 
 LOCAL_MODULE:= libril_static
-
-LOCAL_LDLIBS += -lpthread
 
 include $(BUILD_STATIC_LIBRARY)
 endif # ANDROID_BIONIC_TRANSITION
